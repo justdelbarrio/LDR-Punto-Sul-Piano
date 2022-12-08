9f8fdb4b09a9b0ba465e8d790fc955b9e5176005
Tomas Cutinella <tomas.cutinella05@gmail.com>
2022-12-08 12:05:26 +0100

    Aggiungere i file di progetto.

---
+++ LAVORO PUNTOSUPIANO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace LAVORO_PUNTOSUPIANO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Punto uno = new Punto(1, 4);
+            double x2, y2;
+            Console.WriteLine("Please insert two values (x and y)");
+            
+            x2 = double.Parse(Console.ReadLine());
+            y2 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("");
+            Console.WriteLine(uno.Distanza2Punti(uno.X, uno.Y, x2, y2));
+
+            Thread.Sleep(5000);
+        }
+    }
+}
---
+++ LAVORO PUNTOSUPIANO/Punto.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAVORO_PUNTOSUPIANO
+{
+    public class Punto
+    {
+
+        //ATTRIBUTI 
+
+        private double _x;
+        private double _y;
+
+        //PROPERTIES
+        public Punto(double x, double y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+        public Punto() : this(0,0)
+        {
+        }
+        public double X
+        {
+            get
+            {
+                return _x;
+            }
+            private set
+            {            
+                    _x = value;
+            }
+        }
+        public double Y
+        {
+            get
+            {
+                return _y;
+            }
+            private set
+            {
+                _y = value;
+            }
+        }
+        //METODI
+        public double Distanza2Punti(double x1, double y1, double x2, double y2)
+        {
+            double distanza = Math.Sqrt(Math.Pow((x2-x1),2)+Math.Pow((y2-y1),2));
+            return distanza;
+        }
+
+
+
+
+    }
+}
